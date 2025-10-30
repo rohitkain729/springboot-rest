db3e288ab0a69c4779928bf75b89a030d698cfaf
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2025-10-31 03:11:19 +0530

    Delete 65-springboot-restfull-web-service-1 directory

--- 65-springboot-restfull-web-service-1/src/main/java/com/app/repo/EmpRepo.java
+++
@@ -1,9 +0,0 @@
-package com.app.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.controller.Employee;
-
-public interface EmpRepo extends JpaRepository<Employee, Integer> {
-
-}
